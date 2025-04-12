932d1401dd5571bb143fd1b11027891268e93258
Micheal Owoyemi <120283182+micheal-owoyemi@users.noreply.github.com>
2025-04-12 14:23:35 -0500

    PackShopService Init

---
+++ src/modules/Shared/PackShopServiceTranslator.lua
@@ -0,0 +1,9 @@
+--[[
+	@class PackShopServiceTranslator
+]]
+
+local require = require(script.Parent.loader).load(script)
+
+return require("JSONTranslator").new("PackShopServiceTranslator", "en", {
+	gameName = "FatherKolsGlobalShopServiceNevermore";
+})
